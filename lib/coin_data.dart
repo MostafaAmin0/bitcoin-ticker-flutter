6e69234fd5ddf8d24b3d53064b2a0342bb75a301
m0stafaamin <mostafaamin928@gmail.com>
2021-07-29 09:00:39 +0200

    update ui with api results

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,5 @@
+import 'package:bitcoin_ticker/network.dart';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +30,14 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+
+  final _url='https://rest.coinapi.io/v1/exchangerate/BTC';
+  final _apiKey='A35536F8-C883-4242-8939-9375029A41DD';
+
+  Future getRate(String second)async{
+    NetworkHelper networkHelper= NetworkHelper('${_url}/${second}?apikey=$_apiKey');  
+    return await networkHelper.getData();
+  }
+
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,69 +1,109 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'coin_data.dart';
+import '../components/currencyWidget.dart';
+import 'dart:io' show Platform;
 
 class PriceScreen extends StatefulWidget {
   @override
   _PriceScreenState createState() => _PriceScreenState();
 }
 
-List<DropdownMenuItem> getDropItems(){
-  List<DropdownMenuItem<String>> items=[];
-  for(String currency in currenciesList){
-    items.add(
-      DropdownMenuItem(value: currency,child: Text(currency))
+class _PriceScreenState extends State<PriceScreen> {
+  String dropDownValue = currenciesList[0];
+  CoinData coinData = CoinData();
+  String rate = '?';
+
+  void updateUI() async {
+    try {
+      var data = await coinData.getRate(dropDownValue);
+      setState(() {
+        rate = data['rate'].toStringAsFixed(1);
+        print(rate);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  DropdownButton<String> androidDropDown() {
+    List<DropdownMenuItem<String>> menuItems = [];
+    for (String currency in currenciesList) {
+      menuItems.add(DropdownMenuItem(value: currency, child: Text(currency)));
+    }
+
+    return DropdownButton<String>(
+      items: menuItems,
+      value: dropDownValue,
+      onChanged: (value) {
+        setState(() {
+          dropDownValue = value;
+          updateUI();
+        });
+      },
     );
   }
-  return items;
-}
 
+  CupertinoPicker iosPicker() {
+    List<Text> menuItems = [];
+    for (String currency in currenciesList) {
+      menuItems.add(Text(currency));
+    }
+    return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 30.0,
+      onSelectedItemChanged: (indexValue) {
+        setState(() {
+          dropDownValue = currenciesList[indexValue];
+          updateUI();
+        });
+      },
+      children: menuItems,
+    );
+  }
 
-class _PriceScreenState extends State<PriceScreen> {
-  String dropDownValue=currenciesList[0];
+  @override
+  void initState() {
+    super.initState();
+    updateUI();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('🤑 Coin Ticker'),
+        title: Text(' Coin Ticker'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
+          Column(
+            children: [
+              CurrencyWidget(
+                fromCurrency: cryptoList[0],
+                rate: rate,
+                dropDownValue: dropDownValue,
               ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
+              CurrencyWidget(
+                fromCurrency: cryptoList[1],
+                rate: rate,
+                dropDownValue: dropDownValue,
               ),
-            ),
+              CurrencyWidget(
+                fromCurrency: cryptoList[2],
+                rate: rate,
+                dropDownValue: dropDownValue,
+              ),
+            ],
           ),
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: DropdownButton<String>(
-              items: getDropItems(),
-              value: dropDownValue,
-              onChanged: (value){
-                setState(() {
-                  dropDownValue=value;
-                });
-              },
-            ),
+            child: Platform.isIOS ? iosPicker() : androidDropDown() ,
+            //child: androidDropDown(),
           ),
         ],
       ),
